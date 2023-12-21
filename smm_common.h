d7c6b101232fa09d5dd75da799300b2a4835d281
gayoung46 <gayoung04066@naver.com>
2023-12-21 16:21:31 +0900

    SMMable Project

---
+++ smm_common.h
@@ -0,0 +1,18 @@
+//
+//  smm_common.h
+//  SMMarble
+//
+#ifndef smm_common_h
+#define smm_common_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CHARNAME        200
+
+#define GRADUATE_CREDIT       30
+#define MAX_DIE           6
+#define MAX_PLAYER         100
+
+
+#endif /* smm_common_h */
